dd2a6d2c6123d001e4c216c33875b08f1216a5f1
aedm <korteur@gmail.com>
2022-07-30 14:32:08 +0200

    Craws and uploads to s3

--- crawler/src/main.rs
+++ crawler/src/main.rs
@@ -5,7 +5,7 @@ use aws_config::meta::region::RegionProviderChain;
 use aws_sdk_ec2::model::CapacityReservationInstancePlatform::LinuxUnix;
 use aws_sdk_ec2::model::{
     filter, CapacityReservationInstancePlatform, Filter, InstanceType, OfferingClassType,
-    RiProductDescription, Tenancy,
+    ReservedInstancesOffering, RiProductDescription, Tenancy,
 };
 use aws_sdk_ec2::{Client, Region};
 use aws_sdk_s3::types::ByteStream;
@@ -14,13 +14,14 @@ use dotenv::dotenv;
 use std::time::Duration;
 use std::time::Instant;
 
+const DATE_FORMAT: &str = "%Y%m%d-%H%M%S";
+
 // Uploads a file to S3.
-async fn upload_file(
-    client: &aws_sdk_s3::Client,
-    bucket: &str,
-    key: &str,
-    content: &str,
-) -> Result<()> {
+async fn upload_file(bucket: &str, key: &str, content: &str) -> Result<()> {
+    let region = RegionProviderChain::default_provider().or_else("us-east-1");
+    let shared_config = aws_config::from_env().region(region).load().await;
+    let client: aws_sdk_s3::Client = aws_sdk_s3::Client::new(&shared_config);
+
     let body = Bytes::copy_from_slice(content.as_bytes());
     client
         .put_object()
@@ -29,10 +30,12 @@ async fn upload_file(
         .body(body.into())
         .send()
         .await?;
+
+    println!("Uploaded {}", key);
     Ok(())
 }
 
-async fn show_state(region_name: &str) -> Result<()> {
+async fn show_state(region_name: &str) -> Result<Vec<ReservedInstancesOffering>> {
     let region = Region::new(region_name.to_string());
     let shared_config = aws_config::from_env().region(region).load().await;
     let client = Client::new(&shared_config);
@@ -72,31 +75,39 @@ async fn show_state(region_name: &str) -> Result<()> {
     }
 
     println!("{:#?}", res);
-
-    Ok(())
+    Ok(res)
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let _ = dotenv();
-    let now = Instant::now();
 
-    show_state("us-east-2").await;
-    println!("Elapsed {}", now.elapsed().as_secs());
-    return Ok(());
+    // upload_file("ec2-scraper", "test.txt", "test").await?;
 
-    let regions = [
-        "us-east-1",
-        "us-east-2",
-        "us-west-1",
-        "us-west-2",
-        "eu-central-1",
-    ];
+    // let now = Instant::now();
+    // show_state("us-east-2").await;
+    // println!("Elapsed {}", now.elapsed().as_secs());
 
-    loop {
+    // return Ok(());
+
+    let regions = ["us-east-1", "us-east-2", "us-west-1", "us-west-2"];
+    let regions = ["us-east-1"];
+
+    'foo: loop {
+        let date = chrono::offset::Local::now().format(DATE_FORMAT).to_string();
+        let mut reserved = vec![];
         for region in regions {
-            show_state(region).await;
+            println!("{}", region);
+            let r = show_state(region).await;
+            if let Ok(mut res) = r {
+                reserved.append(&mut res);
+            } else {
+                continue 'foo;
+            }
         }
-        tokio::time::sleep(Duration::from_secs(1)).await;
+        let content = format!("{:#?}", reserved);
+        let file_name = format!("{date}-v1.txt");
+        upload_file("ec2-scraper", &file_name, &content).await?;
+        tokio::time::sleep(Duration::from_secs(60)).await;
     }
 }
